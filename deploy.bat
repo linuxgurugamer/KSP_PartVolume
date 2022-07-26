f01c902550c0e6047775794b49d9567f345d81aa
Jonathan Bayer <linuxgurugamer@gmail.com>
2022-07-26 18:54:52 -0400

    Added patch to add packedVolumeLimit = 0 to those modules which existed and didn't have it

--- deploy.bat
+++ deploy.bat
@@ -8,6 +8,7 @@ rem VERSIONFILE is the name of the version file, usually the same as GAMEDATA,
 rem    but not always
 
 set H=%KSPDIR%
+rem set H=R:\KSP_1.12.3_IFI_US2
 
 set GAMEDIR=KSP_PartVolume
 set GAMEDATA="GameData"
